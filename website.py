7eb4b928657f395d8a6e1d7a7b0b5ac1798630ac
Dylan <dbolter03@gmail.com>
2021-03-12 23:51:50 -0600

    navbar+made layout+added about page

--- website.py
+++ website.py
@@ -2,5 +2,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def main():
     return render_template('main.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
